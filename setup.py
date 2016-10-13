55b058aa95efdace2237d4b93397c8b98c35825a
Viktor M <vityok@gmail.com>
2016-10-13 09:53:47 +0300

    ADD: adding registration files for PYPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'shot',
+  packages = ['shot'], 
+  version = '0.1',
+  description = 'Super micro web framework for Python',
+  author = 'Viktor Moyseyenko',
+  author_email = 'vityok@gmail.com',
+  url = 'https://github.com/2peppers/shot', 
+  download_url = 'https://github.com/2peppers/shot/tarball/0.1', 
+  keywords = ['web', 'wsgi', 'template', 'framework'], 
+  classifiers = [],
+)
